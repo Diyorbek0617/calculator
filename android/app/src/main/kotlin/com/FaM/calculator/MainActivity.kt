c9b1d48eaa4b0fca89d2695b00241fa68fcfa688
Diyorbek0617 <nizomiddinov.d.d@gmail.com>
2022-08-19 11:49:00 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/FaM/calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.FaM.calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:calculator/pages/calculator_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      debugShowCheckedModeBanner: false,
+      home: const Calculator(),
+      routes: {
+        Calculator.id:(context)=>const Calculator(),
+      },
+    );
+  }
+}
